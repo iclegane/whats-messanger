678f4a6394d9a96c88c9a87d2e58a2c8a8b2a430
Viktor <vvs-PathOfExile@yandex.ru>
2023-05-15 17:16:20 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+
+const domNode = document.getElementById('root') as Element;
+const root = ReactDOM.createRoot(domNode);
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+ 
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.css?$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html",
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'src/assets'),
+        },
+        compress: false,
+        open: false,
+        hot: true,
+        port: 8080,
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+};
